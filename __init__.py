c688aed2494521b85f3c3cef7e9337ed93be78c7
Xinyi Lee <1836724126@qq.com>
2025-05-07 22:44:11 +0800

    Bump version to 0.1.12

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 """
 # sequenzo/__init__.py (Top-level)
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 # Lazy import: public submodules
 from sequenzo import (datasets, data_preprocessing, visualization, clustering,
