41954b70e12a2d229e247b0faac7d0aa44e3fd35
Xinyi Lee <1836724126@qq.com>
2025-09-16 23:41:36 +0800

    Build Wheels for Sequenzo.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 """
 # sequenzo/__init__.py (Top-level)
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 # Lazy import: public submodules
 from sequenzo import (datasets, data_preprocessing, visualization, clustering,
