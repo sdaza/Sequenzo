5c9d77467f6f28e1280ba9c09238639fa59385d6
Xinyi Lee <1836724126@qq.com>
2025-09-18 14:01:05 +0800

    Bump version to 0.1.16(Only macos and linux).

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 """
 # sequenzo/__init__.py (Top-level)
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 
 # Lazy import: public submodules
 from sequenzo import (datasets, data_preprocessing, visualization, clustering,
