f9af694b1ce99c7109babd14a375fc9232e83a08
Yuqi Liang <dawson1900@live.com>
2025-09-23 09:46:34 +0200

    Solved the issue of additional space in both index plot and state distribution plot when subplotting is shown

--- sequenzo/visualization/utils/utils.py
+++ sequenzo/visualization/utils/utils.py
@@ -233,7 +233,10 @@ def determine_layout(num_items: int,
 
     # Automatic layout
     if layout == 'column':
-        ncols = 3
+        # Use up to 3 columns, but never exceed the number of items.
+        # This avoids allocating invisible (but space-occupying) subplots
+        # that cause extra blank space on the right when num_items < 3.
+        ncols = int(min(3, max(1, num_items)))
         nrows = (num_items + ncols - 1) // ncols
     elif layout == 'grid':
         ncols = int(np.ceil(np.sqrt(num_items)))
